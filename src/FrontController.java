4b1504075433f734eba4d47edb952da5dbf64c70
tsoa <henintsoapaul@gmail.com>
2024-05-14 08:29:11 +0300

    Get controllers' name, then display.

--- src/FrontController.java
+++ src/FrontController.java
@@ -3,10 +3,17 @@ package src;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import src.annotations.Controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FrontController extends HttpServlet {
+    boolean checked = false;
+    List<String> controllersNames = new ArrayList<>();
+
     @Override
     public void doGet( HttpServletRequest request, HttpServletResponse response )
             throws IOException {
@@ -21,6 +28,35 @@ public class FrontController extends HttpServlet {
 
     protected void processRequest( HttpServletRequest request, HttpServletResponse response )
             throws IOException {
-        response.getWriter().println( request.getRequestURL() );
+        try {
+            if ( !checked ) {
+                // get controllers' package name
+                String packageName = getServletContext().getInitParameter( "app.controllers.packageName" );
+
+                controllersNames = getControllersNames( packageName );
+                checked = true;
+            }
+
+            PrintWriter writer = response.getWriter();
+            for ( String controllerName : controllersNames ) {
+                writer.println( controllerName );
+            }
+        } catch ( ClassNotFoundException e ) {
+            throw new RuntimeException( e );
+        }
+    }
+
+    protected List<String> getControllersNames( String packageName )
+            throws ClassNotFoundException {
+        List<String> controllerNames = new ArrayList<>();
+        Class<?>[] classes = Class.forName( packageName ).getClasses();
+
+        for ( Class<?> clazz : classes ) {
+            if ( clazz.isAnnotationPresent( Controller.class ) ) {
+                String className = clazz.getSimpleName();
+                controllerNames.add( className );
+            }
+        }
+        return controllerNames;
     }
 }
