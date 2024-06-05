af298d5fdc8d4c4da2c112a9f57375c3c7b4426f
tsoa <henintsoapaul@gmail.com>
2024-06-05 10:03:52 +0300

    Throw exception on unsupported return type.

--- src/summer/utils/ScannerUtil.java
+++ src/summer/utils/ScannerUtil.java
@@ -30,39 +30,42 @@ public class ScannerUtil {
 
     public static void scanDirectory( File directory, String packageName, HashMap<String, Mapping> URLMappings )
             throws Exception {
+
+    private static void scanDirectory( File directory, String packageName, HashMap<String, Mapping> URLMappings )
+            throws ClassNotFoundException, ServletException {
         File[] files = directory.listFiles();
         assert files != null;
         for ( File file : files ) {
-            if ( file.isDirectory() ) {
-                scanDirectory( file, packageName + "." + file.getName(), URLMappings );
-            } else {
-                scanFile( file, packageName, URLMappings );
-            }
+            if ( file.isDirectory() ) scanDirectory( file, packageName + "." + file.getName(), URLMappings );
+            else scanFile( file, packageName, URLMappings );
         }
     }
 
-    public static void scanFile( File file, String packageName, HashMap<String, Mapping> URLMappings )
-            throws Exception {
+    private static void scanFile( File file, String packageName, HashMap<String, Mapping> URLMappings )
+            throws ClassNotFoundException, SummerInitException {
         if ( file.getName().endsWith( ".class" ) ) {
             String className = packageName + "." + file.getName().replace( ".class", "" );
             Class<?> clazz = Class.forName( className );
 
-            if ( clazz.isAnnotationPresent( Controller.class ) ) { // Verify if the class is a Controller
+            if ( clazz.isAnnotationPresent( Controller.class ) ) { // Verify if the class is annotated with @Controller
                 for ( Method method : clazz.getDeclaredMethods() ) {
-                    if ( method.isAnnotationPresent( GetMapping.class ) ) { // Verify if the method is a GetMapping
-
-                        String url = method.getAnnotation( GetMapping.class ).urlMapping(),
-                                methodName = method.getName();
+                    if ( method.isAnnotationPresent( GetMapping.class ) ) { // Verify if the method is annotated with @GetMapping
+                        String url = method.getAnnotation( GetMapping.class ).urlMapping(), methodName = method.getName();
 
                         // Verify if the URL is already in the HashMap
                         if ( URLMappings.containsKey( url ) ) {
                             Mapping mapping = URLMappings.get( url );
-                            throw new Exception( "URL \"" + url + "\" already exists in the URLMappings.\n" +
-                                    "Mapping1 -> {\n \tclass: " + mapping.getControllerName() + " \n \tmethod: " + mapping.getMethodName() + " \n}\n" +
-                                    "Mapping2 -> {\n \tclass: " + className + " \n \tmethod: " + methodName + " \n}\n" );
+                            throw new SummerInitException( "\nURL \"" + url + "\" already exists in the URLMappings.\n"
+                                    + "Existing Mapping -> {\n \tclass: " + mapping.getControllerName() + " \n \tmethod: " + mapping.getMethodName() + " \n}\n"
+                                    + "New Mapping -> {\n \tclass: " + className + " \n \tmethod: " + methodName + " \n}\n" );
                         }
 
-                        URLMappings.put( url, new Mapping( className, methodName ) );
+                        // Verify the return type of the method
+                        String returnTypeName = method.getReturnType().getName();
+                        if ( returnTypeName.equals( String.class.getName() ) || returnTypeName.equals( ModelView.class.getName() ) ) {
+                            URLMappings.put( url, new Mapping( className, methodName ) );
+                        } else
+                            throw new SummerInitException( "Unsupported return type \"" + returnTypeName + "\" for method \"" + className + "." + methodName + "()\"" );
                     }
                 }
             }
