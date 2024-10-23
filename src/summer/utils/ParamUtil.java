b139af87a0764d1cb52e93d4c9b5ea4f4e7ce32d
Henintsoa Paul <henintsoapaul@gmail.com>
2024-10-23 09:13:33 +0300

    Dispatch file from form multipart/form-data to the method controller

--- src/summer/utils/ParamUtil.java
+++ src/summer/utils/ParamUtil.java
@@ -1,9 +1,13 @@
 package src.summer.utils;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+
 import src.summer.annotations.Param;
+import src.summer.beans.SummerFile;
 import src.summer.exception.SummerProcessException;
 
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -12,12 +16,12 @@ import java.util.*;
 
 public abstract class ParamUtil {
     /**
-     * Return a list of the values of the args required by the method
-     * that are contained in the request object.
+     * Return a list of the values of the args required by the {@code method}
+     * that are contained in the {@code request} object.
      */
     public static List<Object> getMethodParameterValues( Method method, HttpServletRequest request )
             throws NoSuchFieldException, InvocationTargetException, InstantiationException, IllegalAccessException,
-            NoSuchMethodException, SummerProcessException {
+            NoSuchMethodException, ServletException, IOException {
         List<Object> values = new ArrayList<>();
         for ( Parameter parameter : method.getParameters() ) {
             if ( !parameter.isAnnotationPresent( Param.class ) )
@@ -29,9 +33,18 @@ public abstract class ParamUtil {
 
     public static Object getParameterValue( Parameter parameter, HttpServletRequest request )
             throws NoSuchFieldException, InstantiationException, IllegalAccessException, NoSuchMethodException,
-            InvocationTargetException {
+            InvocationTargetException, ServletException, IOException {
         Param annotation = parameter.getAnnotation( Param.class );
-        String paramName = annotation == null ? parameter.getName() : annotation.name();
+
+        if ( annotation == null )
+            throw new SummerProcessException( "ETU2443 - Parameters must be annotated with \"@Param\"." );
+
+        // todo: Sprint 12
+        String paramName = annotation.name();
+        if ( annotation.isFile() ) {
+            return SummerFile.getFileFromRequest( request, paramName );
+        }
+
         Object paramValue = request.getParameter( paramName );
         Class<?> paramType = parameter.getType();
 
